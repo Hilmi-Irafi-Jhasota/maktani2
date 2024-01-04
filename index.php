6a733227898492271a48987c7dfbe108a63f31b1
HILMI IRAFI JHASOTA <91232813+Hilmi-Irafi-Jhasota@users.noreply.github.com>
2024-01-04 09:16:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,161 @@
+<?php 
+    session_start();
+    require "function/functions.php";
+    
+    if ( isset($_SESSION["login"]) ) {
+        header("Location: dashboard");
+        exit;
+    } elseif(isset($_COOKIE['login'])) {
+        header("Location: dashboard");
+        exit;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="img/icon.jpg">
+    <title>Mak-Tani</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        type="text/css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/modal.css">
+    <style>
+        .parallax {
+            background: url(img/body.jpg);
+            background-attachment: fixed;
+            /* background-size: cover; */
+            background-repeat: no-repeat;
+        }   
+
+        .parallax2 {
+            background: url(img/body.jpg);
+            background-attachment: fixed;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
+</head>
+
+<body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top main-nav" id="mainNav">
+        <div class="container">
+            <a class="js-scroll-trigger" href="#page-top">
+                <img src="img/logo.png" width="20px" style="margin-right: 10px; margin-bottom: 2px;">
+            </a>
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">Mak-Tani</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="login" class="nav-link">Sign in</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Navigation -->
+
+    <!-- header -->
+    <header id="home" class="text-light parallax">
+        <div class="container konten">
+            <h1 style="font-size: 36pt;">Welcome to Mak-Tani</h1>
+            <p class="lead" style="font-size: 16pt;">Manajemen keuangan Tani</p>
+            <a href="login" class="btn btn-outline-light button">login</a>
+        </div>
+    </header>
+    <!-- header -->
+    
+    <!-- Footer -->
+    <div class="py-3 bg-dark">
+        <div class="container text-light">
+            <div class="row">
+                <div class="col-lg-3 col-6 p-3">
+                    <h5> <b>Main</b> </h5>
+                    <ul class="list-unstyled">
+                        <li> <a href="#home" class="js-scroll-trigger foot-link">Home</a> </li>
+                        <li> <a href="#features" class="js-scroll-trigger foot-link">Features</a> </li>
+                        <li> <a href="#about" class="js-scroll-trigger foot-link">About Us</a> </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-6 p-3">
+                    <h5> <b>Others</b> </h5>
+                    <ul class="list-unstyled">
+                        <li> <a href="faq" class="foot-link">FAQ</a> </li>
+                        <li> <a href="#" class="foot-link">Promotion Videos</a> </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6 p-3">
+                    <h5> <b>About</b> </h5>
+                    <p class="mb-0">Aplikasi Mak-Tani dilengkapi dengan fitur menarik yang dapat mempermudah petani mengelola keuangannya.</p>
+                </div>
+                <div class="col-lg-3 col-md-6 p-3">
+                    <h5> <b>Follow us</b> </h5>
+                    <div class="row">
+                        <div class="col-md-12 d-flex align-items-center justify-content-between my-2">
+                            <a href="https://www.facebook.com" class="foot-link" target="_blank">
+                                <i class="d-block fa fa-facebook-official warna-icon fa-lg mr-2"></i>
+                            </a>
+                            <a href="https://www.instagram.com" class="foot-link" target="_blank">
+                                <i class="d-block fa fa-instagram warna-icon fa-lg mx-2"></i>
+                            </a>
+                            <a href="https://plus.google.com" class="foot-link" target="_blank">
+                                <i class="d-block fa fa-google-plus-official warna-icon fa-lg mx-2"></i>
+                            </a>
+                            <a href="https://www.pinterest.com" class="foot-link" target="_blank">
+                                <i class="d-block fa fa-pinterest-p warna-icon fa-lg mx-2"></i>
+                            </a>
+                            <a href="https://www.reddit.com" class="foot-link" target="_blank">
+                                <i class="d-block fa fa-reddit warna-icon fa-lg mx-2"></i>
+                            </a>
+                            <a href="https://twitter.com" class="foot-link" target="_blank">
+                                <i class="d-block fa fa-twitter warna-icon fa-lg ml-2"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <p class="mb-0 mt-2">Copyright © 2022 All rights reserved.</p> 
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer -->
+
+    <!-- <footer class="bg-dark foot">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; 2018 Semicolon SQUAD</p>
+        </div>
+    </footer> -->
+
+    <!-- js utama -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- js scrolling -->
+    <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
